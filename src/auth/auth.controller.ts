16d0fcb2fc50664360d24394184ce4b5254b908b
younha00 <ayeon0@office.skhu.ac.kr>
2025-06-14 02:08:58 +0900

    chore: ci 해결용 never used 변수 정리

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,8 @@
-import { AuthService } from '@/auth/auth.service';
-import { AuthSignInDto, AuthSignUpDto } from '@/auth/dto/request';
-import { AuthTokenResponse } from '@/auth/dto/response';
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor() {}
 }
