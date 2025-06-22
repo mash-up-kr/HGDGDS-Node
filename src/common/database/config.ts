27e48f44d6697f246ea57e9bbd36613dda1f0574
hye-on <ain0103@naver.com>
2025-06-22 16:17:00 +0900

    chore: 개발 환경에 따라서 ssl 연결 설정 다르게 수정

--- src/common/database/config.ts
+++ src/common/database/config.ts
@@ -18,9 +18,11 @@ export const typeormConfig: DataSourceOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isDev
+    ? false
+    : {
+        rejectUnauthorized: false,
+      },
   synchronize: isDev,
   logging: isDev,
   entities: [join(cwd(), 'dist', '**', `*.entity.js`)],
