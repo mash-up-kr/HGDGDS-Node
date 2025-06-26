1ffc694d5d9355e69733c9f5b5e63cfad1835d57
hye-on <ain0103@naver.com>
2025-06-26 19:33:24 +0900

    feat: 회원가입 및 로그인 API 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { JwtPayload } from '@/common';
+
 import { UsersRepository } from '@/users/users.repository';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { SignUpRequestDto } from './dto/request/sign-up.request.dto';
+import { SignUpResponseDto } from './dto/response/sign-up.response';
+import { CreateUserDto } from '@/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -15,5 +19,28 @@ export class AuthService {
       secret: process.env.JWT_SECRET ?? '',
       expiresIn: process.env.JWT_EXPIRES_IN ?? '6h',
     });
+  }
+
+  async signUp(signUpDto: SignUpRequestDto): Promise<SignUpResponseDto> {
+    const { deviceId, nickname, profileImageCode } = signUpDto;
+
+    let user = await this.userRepository.findUserByDeviceId(deviceId);
+
+    if (!user) {
+      const createUserDto = new CreateUserDto(
+        deviceId,
+        nickname,
+        profileImageCode,
+      );
+      user = await this.userRepository.createUser(createUserDto);
+    }
+
+    const payload: JwtPayload = {
+      id: user.id,
+      role: user.role,
+    };
+    const accessToken = await this.issueAccessToken(payload);
+
+    return new SignUpResponseDto(user, accessToken);
   }
 }
