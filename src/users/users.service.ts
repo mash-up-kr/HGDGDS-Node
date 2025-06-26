6560f959a4c2a3430ffde19cc2f0b84549b61cba
hye-on <ain0103@naver.com>
2025-06-26 22:02:28 +0900

    feat: 사용자 설정 변경 API 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { UpdateUserSettingsRequestDto } from './dto/request/update-user-settings.dto';
+import { UpdateUserSettingsResponseDto } from './dto/response/update-user-settings.response';
+import { UserNotFoundException } from '@/common/exception/user.exception';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
+
+  async updateUserSettings(
+    userId: number,
+    updateDto: UpdateUserSettingsRequestDto,
+  ): Promise<UpdateUserSettingsResponseDto> {
+    const existingUser = await this.usersRepository.findUserById(userId);
+
+    if (!existingUser) {
+      throw new UserNotFoundException();
+    }
+
+    const updatedUser = await this.usersRepository.updateUserSettings(
+      userId,
+      updateDto,
+    );
+
+    return new UpdateUserSettingsResponseDto(updatedUser);
+  }
 }
