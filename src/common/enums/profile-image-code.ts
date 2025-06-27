1c06cf61ddbef3d1949a684c3cd294cb58c49e30
hye-on <ain0103@naver.com>
2025-06-27 20:57:16 +0900

    chore: 회원가입때 수정한것. 여기서도 필요해서 가져옴

--- src/common/enums/profile-image-code.ts
+++ src/common/enums/profile-image-code.ts
@@ -5,3 +5,17 @@ export enum ProfileImageCode {
   IMG_004 = 'IMG_004',
   IMG_005 = 'IMG_005',
 }
+
+export const PROFILE_IMAGE_PATH_MAP: Record<ProfileImageCode, string> = {
+  [ProfileImageCode.IMG_001]: 'profile-images/IMG_001.png',
+  [ProfileImageCode.IMG_002]: 'profile-images/IMG_002.png',
+  [ProfileImageCode.IMG_003]: 'profile-images/IMG_003.png',
+  [ProfileImageCode.IMG_004]: 'profile-images/IMG_004.png',
+  [ProfileImageCode.IMG_005]: 'profile-images/IMG_005.png',
+};
+
+export const getProfileImagePath = (
+  profileImageCode: ProfileImageCode,
+): string => {
+  return PROFILE_IMAGE_PATH_MAP[profileImageCode];
+};
