a796b6f18c6ab2938bbd8d204c9363027a5969b2
younha00 <ayeon0@office.skhu.ac.kr>
2025-06-27 15:14:46 +0900

    진짜 마지막....모킹함수 @로 변경, jest 설정 package.json로 통일

--- src/firebase/firebase.service.spec.ts
+++ src/firebase/firebase.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FirebaseService } from './firebase.service';
 
-jest.mock('src/firebase/firebase.config', () => ({
+jest.mock('@/firebase/firebase.config', () => ({
   __esModule: true,
   default: jest.fn(() => ({})),
 }));
