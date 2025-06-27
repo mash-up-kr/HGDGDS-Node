c8efd4bc90c69b0d2d2b05b2ef175e91b2076000
hye-on <ain0103@naver.com>
2025-06-27 18:59:56 +0900

    chore: 리뷰 반영. 프로필이미지 presignedURL로 내려주기

--- src/common/enums/profile-image-code.ts
+++ src/common/enums/profile-image-code.ts
@@ -6,21 +6,16 @@ export enum ProfileImageCode {
   IMG_005 = 'IMG_005',
 }
 
-export const PROFILE_IMAGE_URL_MAP: Record<ProfileImageCode, string> = {
-  [ProfileImageCode.IMG_001]:
-    'https://kr.object.ncloudstorage.com/app-images/assets/img_profile_01.png',
-  [ProfileImageCode.IMG_002]:
-    'https://kr.object.ncloudstorage.com/app-images/assets/img_profile_02.png',
-  [ProfileImageCode.IMG_003]:
-    'https://kr.object.ncloudstorage.com/app-images/assets/img_profile_03.png',
-  [ProfileImageCode.IMG_004]:
-    'https://kr.object.ncloudstorage.com/app-images/assets/img_profile_04.png',
-  [ProfileImageCode.IMG_005]:
-    'https://kr.object.ncloudstorage.com/app-images/assets/img_profile_05.png',
+export const PROFILE_IMAGE_PATH_MAP: Record<ProfileImageCode, string> = {
+  [ProfileImageCode.IMG_001]: 'profile-images/IMG_001.png',
+  [ProfileImageCode.IMG_002]: 'profile-images/IMG_002.png',
+  [ProfileImageCode.IMG_003]: 'profile-images/IMG_003.png',
+  [ProfileImageCode.IMG_004]: 'profile-images/IMG_004.png',
+  [ProfileImageCode.IMG_005]: 'profile-images/IMG_005.png',
 };
 
-export const getProfileImageUrl = (
+export const getProfileImagePath = (
   profileImageCode: ProfileImageCode,
 ): string => {
-  return PROFILE_IMAGE_URL_MAP[profileImageCode];
+  return PROFILE_IMAGE_PATH_MAP[profileImageCode];
 };
