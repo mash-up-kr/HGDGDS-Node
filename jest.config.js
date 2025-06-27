684c8802d10ef336fa93b32b17cdb12eb9ff37c4
younha00 <ayeon0@office.skhu.ac.kr>
2025-06-27 14:51:09 +0900

    ci 테스트용 경로 분리, root dir 속성 삭제

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
