3c4493f1e2db35fa513a95c23fecb54a3e3d10de
younha00 <ayeon0@office.skhu.ac.kr>
2025-06-29 00:13:46 +0900

    feat: fcm 테스트 api 구현

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
+import { FcmTestDto } from '@/firebase/dto/firebase-test.dto';
 
+@ApiTags('firebase')
 @Controller('firebase')
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) {}
+
+  @Post('test')
+  @ApiOperation({ summary: 'FCM 테스트 알림 전송' })
+  @ApiBody({ type: FcmTestDto })
+  @ApiResponse({ status: 200, description: '알림 전송 성공' })
+  async sendTestNotification(@Body() dto: FcmTestDto) {
+    return await this.firebaseService.sendNotification(
+      dto.token,
+      '콕 찌르기가 활성화 되었어요!',
+      '콕 찔러서 친구들에게 알림을 보내보세요',
+      '',
+    );
+  }
 }
