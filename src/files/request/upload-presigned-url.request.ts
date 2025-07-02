b2a812a89e3eae965a8a2455ce89b3c304b431de
daisy-kim <sally0226@naver.com>
2025-07-02 22:51:22 +0900

    Edit file prefix
    - reservation/info 의 경우, 이미지 업로드 시점에 reservation_id를 알 수 없기 때문에 path에서 reservation_id를 제거함

--- src/files/request/upload-presigned-url.request.ts
+++ src/files/request/upload-presigned-url.request.ts
@@ -1,17 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-import { IsValidFilePrefix } from '../validator/file-prefix.validator';
+import { IsIn, IsString } from 'class-validator';
 
 export class UploadPresignedUrlRequest {
   @ApiProperty({
     description: `파일 종류 (파일 저장 경로의 prefix)
-- 예약 정보: /reservations/{reservation_id}/info
-- 예약 결과: /reservations/{reservation_id}/result`,
-    example: '/reservations/{reservation_id}/info',
+- 예약 정보: /reservations/info
+- 예약 결과: /reservations/result`,
+    example: '/reservations/info',
   })
   @IsString()
-  @IsValidFilePrefix()
+  @IsIn(['/reservations/info', '/reservations/result'])
   filePrefix: string;
+
   @ApiProperty({
     description: '파일의 확장자',
     example: 'jpeg, png, ...',
