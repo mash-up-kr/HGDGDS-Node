ddc2f265fc0fffaac2cb7f4728bf2fe111f604f6
hye-on <ain0103@naver.com>
2025-07-09 21:00:50 +0900

    feat: AASA 파일 서빙을 ServeStaticModule로 변경

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,10 @@ import { AppModule } from '@/app.module';
 import { swaggerConfig } from '@/app.swagger';
 import { nestConfig } from '@/app.config';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
-import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
-  app.useStaticAssets(join(__dirname, '..', 'public'), {
-    prefix: '/',
-  });
 
   nestConfig(app);
   swaggerConfig(app);
