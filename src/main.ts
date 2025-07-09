cb5788cabda286ff6f86bd5dce565478a97883dd
hye-on <ain0103@naver.com>
2025-07-09 20:39:54 +0900

    feat: 유니버셜 링크를 위한 AASA추가

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { AppModule } from '@/app.module';
 import { swaggerConfig } from '@/app.swagger';
 import { nestConfig } from '@/app.config';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/',
+  });
+
   nestConfig(app);
   swaggerConfig(app);
 
