abc690be57ef298c024f3af882fe42c5076bd7b6
hye-on <ain0103@naver.com>
2025-07-10 01:23:53 +0900

    fix: 정적파일 컨트롤러로 tnwjd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from '@/app.service';
 import { Public } from '@/common/decorator/public.decorator';
 
@@ -10,5 +10,32 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('.well-known/apple-app-site-association')
+  @Header('Content-Type', 'application/json')
+  getAppleAppSiteAssociation() {
+    return {
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appIDs: ['V2YNV9QV27.HGDGDS.HGDGDS-iOS'],
+            components: [
+              {
+                '/': '/invite',
+                comment: 'Matches /invite path for reservation invitations',
+              },
+              {
+                '#': 'no_universal_links',
+                exclude: true,
+                comment:
+                  'Matches any URL with a fragment that equals no_universal_links',
+              },
+            ],
+          },
+        ],
+      },
+    };
   }
 }
--- src/common/interceptor/response.interceptor.ts
+++ src/common/interceptor/response.interceptor.ts
@@ -5,11 +5,18 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
+import { Request } from 'express';
 import { CommonResponse } from '../response/common.response';
 
 @Injectable()
 export class CommonResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    if (request.url === '/.well-known/apple-app-site-association') {
+      return next.handle();
+    }
+
     return next.handle().pipe(
       map((data) => {
         return new CommonResponse(200, 'OK', data);
