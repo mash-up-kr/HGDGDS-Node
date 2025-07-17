864f78df8b8caba00e5155fc00576ea692bed4bd
daisy-kim <sally0226@naver.com>
2025-07-17 21:27:27 +0900

    Add access log

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from '@/app.module';
 import { swaggerConfig } from '@/app.swagger';
 import { nestConfig } from '@/app.config';
 import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useLogger(app.get(Logger));
 
   nestConfig(app);
   swaggerConfig(app);
